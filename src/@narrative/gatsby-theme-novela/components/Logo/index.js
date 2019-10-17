116c896c98fbc36c4b88662d6761e153754f3fad
Puneet <puneettiwari61@gmail.com>
2019-10-17 11:39:43 +0530

    logo

---
+++ src/@narrative/gatsby-theme-novela/components/Logo/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import {Link} from 'gatsby';
+
+export default function Logo() {
+    return(
+        <link to="/"><h1 style={{fontSize: '2.8rem'}}>Puneet</h1></link>
+    );
+}
